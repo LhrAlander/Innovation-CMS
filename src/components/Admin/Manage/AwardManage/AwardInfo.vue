fd94514f31cbf36f8403b084f150ce2849fe3f1f
LhrAlander <1147816814@qq.com>
2018-02-02 18:03:21 +0800

    完成获奖信息的操作

--- src/components/Admin/Manage/AwardManage/AwardInfo.vue
+++ src/components/Admin/Manage/AwardManage/AwardInfo.vue
@@ -81,320 +81,345 @@
   </div>
 </template>
 <script>
-  import axios from 'axios'
-  import ElButton from "../../../../../node_modules/element-ui/packages/button/src/button.vue";
-  import FilterBox from "components/Admin/Manage/FilterBox"
-  import InfoAdd from "components/Admin/Manage/InfoAdd"
-  import * as utils from 'utils/utils'
-  export default {
-    components: {ElButton, FilterBox, InfoAdd},
-    data() {
-      return {
-        tableData: [{ // 表格数据
+import axios from "axios";
+import FilterBox from "components/Admin/Manage/FilterBox";
+import InfoAdd from "components/Admin/Manage/InfoAdd";
+import * as utils from "utils/utils";
+export default {
+  components: { FilterBox, InfoAdd },
+  data() {
+    return {
+      tableData: [
+        {
+          // 表格数据
           id: 1,
-          awardName: '搞笑奖', //获奖名称
-          awardLevel: '国家级',//获奖等级
-          awardSecondLevel: '一等奖', // 一等奖
-          awardTime: '2099-9-9',//获奖时间
+          awardName: "搞笑奖", //获奖名称
+          awardLevel: "国家级", //获奖等级
+          awardSecondLevel: "一等奖", // 一等奖
+          awardTime: "2099-9-9" //获奖时间
         }
+      ],
+      valueLabelMap: {
+        // 下拉类型的input的中值和标签的映射表
+        //          role: [{ // 用户类别映射表
+        //            value: 0,
+        //            label: '全部'
+        //          }, {
+        //            value: 1,
+        //            label: '学生'
+        //          }, {
+        //            value: 2,
+        //            label: '老师'
+        //          }, {
+        //            value: 3,
+        //            label: '企业'
+        //          }],
+        awardLevel: [
+          {
+            value: 0,
+            label: "级别0"
+          },
+          {
+            value: 1,
+            label: "级别1"
+          }
         ],
-        valueLabelMap: { // 下拉类型的input的中值和标签的映射表
-//          role: [{ // 用户类别映射表
-//            value: 0,
-//            label: '全部'
-//          }, {
-//            value: 1,
-//            label: '学生'
-//          }, {
-//            value: 2,
-//            label: '老师'
-//          }, {
-//            value: 3,
-//            label: '企业'
-//          }],
-          awardLevel: [{
+        awardSecondLevel: [
+          {
             value: 0,
-            label: '级别0'
-          },{
+            label: "级别0"
+          },
+          {
             value: 1,
-            label: '级别1'
-          }],
-          awardSecondLevel: [{
-            value: 0,
-            label: '级别0'
-          },{
-            value: 1,
-            label: '级别1'
-          }],
-        },
-
-        keyFormatMap: { // 格式化标签映射表
-          awardName: '获奖名称',
-          awardLevel: '获奖等级',
-          awardSecondLevel: '获奖级别',
-          awardTime: '获奖时间',
-
-        },
-        expandFormatMap: { // 格式化额外信息映射表
-        },
-        infoAddTmpl: {
-          awardName: {
-            label: '获奖名称',
-            inputType: 0, // 0 代表 input
-          },
-          awardLevel: {
-            label: '获奖等级',
-            inputType: 1, // 0 代表 input
-          },
-          awardSecondLevel: {
-            label: '获奖级别',
-            inputType: 1, // 0 代表 input
-          },
-          awardTime: {
-            label: '获奖时间',
-            inputType: 2,
-          },
-        },
-        infoAddRules: {
-          awardName: [
-            {required: true, message: '请输入获奖名称', trigger: 'blur'}
-          ],
-          awardLevel: [
-            {required: true, message: '请输入获奖等级', trigger: 'blur'}
-          ],
-          awardSecondLevel: [
-            {required: true, message: '请输入获奖级别', trigger: 'blur'}
-          ],
-          awardTime: [
-            {type: 'date', required: true, message: '请选择日期', trigger: 'change'}
-          ]
-        },
-//        获取表格数据的地址
-        url: '',
-        filterTmpl: {
-          awardName: {
-            label: '获奖名称',
-            inputType: 0, // 0 代表 input
-          },
-          awardLevel: {
-            label: '获奖等级',
-            inputType: 1, // 0 代表 input
-          },
-          awardSecondLevel: {
-            label: '获奖等级',
-            inputType: 1, // 0 代表 input
-          },
-          awardTime: {
-            label: '获奖时间',
-            inputType: 2,
-          },
-        },
-        filter: { //搜索条件
-          awardName: '', //获奖名称
-          awardLevel: '',//获奖等级
-          awardSecondLevel: '',//获奖级别
-          awardTime: '',//获奖时间
-        },
-        pageSize: 15, //每页大小
-        currentPage: 1, //当前页
-        start: 1, //查询的页码
-        totalCount: 30, //返回的记录总数
-        showFilterBox: false, // 是否显示筛选框
-        tagEmpty: true, //标签是否为空
-        showInfoAdd: false, // 是否显示信息添加框
-      }
-    },
-    mounted: function(){
-      this.loadData(this.filter, this.currentName, this.pageSize);
-    },
-    methods: {
-      getRowKeys(row) {
-        return row.id;
+            label: "级别1"
+          }
+        ]
       },
-//        异步加载数据
-      loadData(filter, pageNum, pageSize) {
-        axios.get(this.url, {param: filter, pageNum: pageNum, pageSize: pageSize})
-          .then(function (res) {
+
+      keyFormatMap: {
+        // 格式化标签映射表
+        awardName: "获奖名称",
+        awardLevel: "获奖等级",
+        awardSecondLevel: "获奖级别",
+        awardTime: "获奖时间"
+      },
+      expandFormatMap: {
+        // 格式化额外信息映射表
+      },
+      infoAddTmpl: {
+        awardName: {
+          label: "获奖名称",
+          inputType: 0 // 0 代表 input
+        },
+        awardLevel: {
+          label: "获奖等级",
+          inputType: 1 // 0 代表 input
+        },
+        awardSecondLevel: {
+          label: "获奖级别",
+          inputType: 1 // 0 代表 input
+        },
+        awardTime: {
+          label: "获奖时间",
+          inputType: 2
+        }
+      },
+      infoAddRules: {
+        awardName: [
+          { required: true, message: "请输入获奖名称", trigger: "blur" }
+        ],
+        awardLevel: [
+          { required: true, message: "请输入获奖等级", trigger: "blur" }
+        ],
+        awardSecondLevel: [
+          { required: true, message: "请输入获奖级别", trigger: "blur" }
+        ],
+        awardTime: [
+          {
+            type: "date",
+            required: true,
+            message: "请选择日期",
+            trigger: "change"
+          }
+        ]
+      },
+      //        获取表格数据的地址
+      url: "",
+      filterTmpl: {
+        awardName: {
+          label: "获奖名称",
+          inputType: 0 // 0 代表 input
+        },
+        awardLevel: {
+          label: "获奖等级",
+          inputType: 1 // 0 代表 input
+        },
+        awardSecondLevel: {
+          label: "获奖等级",
+          inputType: 1 // 0 代表 input
+        },
+        awardTime: {
+          label: "获奖时间",
+          inputType: 2
+        }
+      },
+      filter: {
+        //搜索条件
+        awardName: "", //获奖名称
+        awardLevel: "", //获奖等级
+        awardSecondLevel: "", //获奖级别
+        awardTime: "" //获奖时间
+      },
+      pageSize: 15, //每页大小
+      currentPage: 1, //当前页
+      start: 1, //查询的页码
+      totalCount: 30, //返回的记录总数
+      showFilterBox: false, // 是否显示筛选框
+      tagEmpty: true, //标签是否为空
+      showInfoAdd: false // 是否显示信息添加框
+    };
+  },
+  mounted: function() {
+    axios.get("/api/award/awards")
+      .then(res => {
+        res = res.data;
+        if (res.code == 200) {
+          console.log(res);
+        }
+      });
+    this.loadData(this.filter, this.currentName, this.pageSize);
+  },
+  methods: {
+    getRowKeys(row) {
+      return row.id;
+    },
+    //        异步加载数据
+    loadData(filter, pageNum, pageSize) {
+      axios
+        .get(this.url, { param: filter, pageNum: pageNum, pageSize: pageSize })
+        .then(
+          function(res) {
             this.tableData = res.data.pagesTableData;
             this.totalCount = res.data.number;
-          }, function () {
-            console.log('failed')
-          }).catch(function (err) {
-          console.log(err)
-        })
-      },
-      unique(array) {
-        var r = [];
-        for(var i = 0, l = array.length; i < l; i++) {
-          for(var j = i + 1; j < l; j++)
-            if (array[i] === array[j]) j = ++i;
-          r.push(array[i]);
-        }
-        return r;
-      },
-
-      handleMore(index, row) {
-      },
-//        删除按钮事件
-      handleDelete(index, row) {
-        var array = [];
-        array.push(row.id);
-        axios.post('',{"array":array},{emulateJson: true})
-          .then(function (res) {
-            this.loadData(this.filter, this.currentPage, this.pageSize);
-          }, function () {
-            console.log('failed');
-          })
-      },
-//        编辑按钮事件
-      handleEdit(index, row) {
-        console.log(index, row)
-      },
-//        单页大小改变回调事件
-      handleSizeChange(val) {
-        this.pageSize = val;
-        this.loadData(this.filter, this.currentName, this.pageSize);
-      },
-//        当前页改变回调事件
-      handleCurrentChange(val) {
-        this.currentPage = val;
-        this.loadData(this.filter, this.currentName, this.pageSize);
-      },
-//        点击筛选触发的事件
-      enterFilter() {
-        this.showFilterBox = true;
-      },
-//        接收子组件filterbox传递的筛选条件数据
-      receiveFilter(filter) {
-        if (filter !== undefined)
-          this.filter = filter;
-        this.showFilterBox = false;
-        this.loadData(this.filter, this.currentName, this.pageSize);
-      },
-//        标签的key格式化器
-      keyFormater: function(value) {
-        if (!value) return '';
-        value = value.toString();
-        return Object.assign({},this.keyFormatMap,this.expandFormatMap)[value];
-      },
-      resetObject: utils.resetObject,
-      valueFormater: utils.valueFormater,
-      quitFilter: function () {
-        this.filter = this.resetObject(this.filter);
-        this.loadData(this.filter, this.currentName, this.pageSize);
-      },
-      enterAdd: function () {
-        this.showInfoAdd = true;
-      },
-      receiveInfo: function (data) {
-        if (data) {
-          axios.post("",{'data':data}, {emulateJson: true})
-            .then(function (res) {
-              this.loadData(this.filter, this.currentPage, this.pageSize);
-            }, function () {
-              console.log('failed');
-            })
-        }
-        this.showInfoAdd = false;
-
-      }
-
-    },
-    watch: {
-      filter: {
-        handler: function (val) {
-          this.tagEmpty = true;
-          for (let item in val) {
-            if (val[item] !== '') {
-              this.tagEmpty = false;
-              break;
-            }
+          },
+          function() {
+            console.log("failed");
           }
-        },
-        deep: true
+        )
+        .catch(function(err) {
+          console.log(err);
+        });
+    },
+    unique(array) {
+      var r = [];
+      for (var i = 0, l = array.length; i < l; i++) {
+        for (var j = i + 1; j < l; j++) if (array[i] === array[j]) j = ++i;
+        r.push(array[i]);
       }
+      return r;
+    },
+
+    handleMore(index, row) {},
+    //        删除按钮事件
+    handleDelete(index, row) {
+      var array = [];
+      array.push(row.id);
+      axios.post("", { array: array }, { emulateJson: true }).then(
+        function(res) {
+          this.loadData(this.filter, this.currentPage, this.pageSize);
+        },
+        function() {
+          console.log("failed");
+        }
+      );
+    },
+    //        编辑按钮事件
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    //        单页大小改变回调事件
+    handleSizeChange(val) {
+      this.pageSize = val;
+      this.loadData(this.filter, this.currentName, this.pageSize);
+    },
+    //        当前页改变回调事件
+    handleCurrentChange(val) {
+      this.currentPage = val;
+      this.loadData(this.filter, this.currentName, this.pageSize);
+    },
+    //        点击筛选触发的事件
+    enterFilter() {
+      this.showFilterBox = true;
+    },
+    //        接收子组件filterbox传递的筛选条件数据
+    receiveFilter(filter) {
+      if (filter !== undefined) this.filter = filter;
+      this.showFilterBox = false;
+      this.loadData(this.filter, this.currentName, this.pageSize);
+    },
+    //        标签的key格式化器
+    keyFormater: function(value) {
+      if (!value) return "";
+      value = value.toString();
+      return Object.assign({}, this.keyFormatMap, this.expandFormatMap)[value];
+    },
+    resetObject: utils.resetObject,
+    valueFormater: utils.valueFormater,
+    quitFilter: function() {
+      this.filter = this.resetObject(this.filter);
+      this.loadData(this.filter, this.currentName, this.pageSize);
+    },
+    enterAdd: function() {
+      this.showInfoAdd = true;
+    },
+    receiveInfo: function(data) {
+      if (data) {
+        axios.post("", { data: data }, { emulateJson: true }).then(
+          function(res) {
+            this.loadData(this.filter, this.currentPage, this.pageSize);
+          },
+          function() {
+            console.log("failed");
+          }
+        );
+      }
+      this.showInfoAdd = false;
+    }
+  },
+  watch: {
+    filter: {
+      handler: function(val) {
+        this.tagEmpty = true;
+        for (let item in val) {
+          if (val[item] !== "") {
+            this.tagEmpty = false;
+            break;
+          }
+        }
+      },
+      deep: true
     }
   }
+};
 </script>
 <style scoped>
-  .demo-table-expand {
-    font-size: 0;
-  }
-  .demo-table-expand label {
-    width: 90px;
-    color: #99a9bf;
-  }
-  .demo-table-expand .el-form-item {
-    margin-right: 0;
-    margin-bottom: 0;
-    width: 50%;
-  }
+.demo-table-expand {
+  font-size: 0;
+}
+.demo-table-expand label {
+  width: 90px;
+  color: #99a9bf;
+}
+.demo-table-expand .el-form-item {
+  margin-right: 0;
+  margin-bottom: 0;
+  width: 50%;
+}
 
-  .wrap {
-    position: relative;
-    padding: 40px 50px;
-  }
-  .addInfo {
-    float: right;
-    margin-right: 40px;
-    margin-bottom: 20px;
-  }
-  .filter {
-    float: right;
-    margin-right: 20px;
-    background-color: #9B59B6;
-    color: #ECF0F1;
-    outline: 0;
-    border: 1px solid #9B59B6;
-  }
-  .filter:hover {
-    opacity: .7;
-  }
-  .filter:active {
-    opacity: 1;
-    background-color: #71468B;
-  }
-  .exit-filter {
-    float: right;
-    margin-right: 20px;
-    background-color: #f19500;
-    color: #ECF0F1;
-    outline: 0;
-    border: 1px solid #f19500;
-  }
-  .exit-filter:hover {
-    opacity: .7;
-  }
-  .exit-filter:active {
-    opacity: 1;
-    background-color: #c77800;
-  }
-  .pagination {
-    float: right;
-    margin-top: 20px;
-  }
-  .edit-btn {
-    background-color: #5CB85C;
-    color: #ECF0F1;
-    outline: 0;
-    border: 1px solid #5CB85C;
-  }
-  .edit-btn:hover {
-    opacity: .7;
-  }
-  .edit-btn:active {
-    opacity: 1;
-    background-color: #4E9B4E;
-  }
+.wrap {
+  position: relative;
+  padding: 40px 50px;
+}
+.addInfo {
+  float: right;
+  margin-right: 40px;
+  margin-bottom: 20px;
+}
+.filter {
+  float: right;
+  margin-right: 20px;
+  background-color: #9b59b6;
+  color: #ecf0f1;
+  outline: 0;
+  border: 1px solid #9b59b6;
+}
+.filter:hover {
+  opacity: 0.7;
+}
+.filter:active {
+  opacity: 1;
+  background-color: #71468b;
+}
+.exit-filter {
+  float: right;
+  margin-right: 20px;
+  background-color: #f19500;
+  color: #ecf0f1;
+  outline: 0;
+  border: 1px solid #f19500;
+}
+.exit-filter:hover {
+  opacity: 0.7;
+}
+.exit-filter:active {
+  opacity: 1;
+  background-color: #c77800;
+}
+.pagination {
+  float: right;
+  margin-top: 20px;
+}
+.edit-btn {
+  background-color: #5cb85c;
+  color: #ecf0f1;
+  outline: 0;
+  border: 1px solid #5cb85c;
+}
+.edit-btn:hover {
+  opacity: 0.7;
+}
+.edit-btn:active {
+  opacity: 1;
+  background-color: #4e9b4e;
+}
 
-  .tagBlock {
-    display: inline-block;
-    margin-top: 10px;
-    margin-left: 20%;
-
-  }
-  .tag {
-    margin: 5px;
-  }
+.tagBlock {
+  display: inline-block;
+  margin-top: 10px;
+  margin-left: 20%;
+}
+.tag {
+  margin: 5px;
+}
 </style>
 
